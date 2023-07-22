8d7f4f38e0cafa42a45015ff254d3e633d866ef1
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2023-07-22 14:59:06 +0900

    change s3 controller

--- demo/src/main/java/simple_api/demo/Controller/uploadController.java
+++ demo/src/main/java/simple_api/demo/Controller/uploadController.java
@@ -7,37 +7,44 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @RestController
-@RequestMapping("/upload")
 @RequiredArgsConstructor
 public class uploadController {
-    private final AmazonS3Client amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
-    private String bucket;
+    private  String bucket;
 
-    @PostMapping
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file) {
-        try {
-            String fileName=file.getOriginalFilename();
-            String fileUrl= "https://" + bucket + "/test" +fileName;
-            ObjectMetadata metadata= new ObjectMetadata();
+    private final AmazonS3Client amazonS3Client;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file){
+        try{
+            String fileName = UUID.randomUUID()+"-"+file.getOriginalFilename();
+
+            ObjectMetadata metadata = new ObjectMetadata();
             metadata.setContentType(file.getContentType());
             metadata.setContentLength(file.getSize());
+
+            String fileUrl=amazonS3Client.getUrl(bucket,fileName).toString();
+
             amazonS3Client.putObject(bucket,fileName,file.getInputStream(),metadata);
             return ResponseEntity.ok(fileUrl);
-        } catch (IOException e) {
+        }catch(IOException e){
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
+
+
+
+
 
 
 }
